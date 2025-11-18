b2d25d07c62b5a1bb6c6ae62b97a0dc99d79a02c
Rituraj0191 <146822205+Rituraj0191@users.noreply.github.com>
2025-11-19 00:26:10 +0530

    Add files via upload

---
+++ Task_7.py
@@ -0,0 +1,10 @@
+
+print("Reading file content :")
+
+try:
+    with open("sample.txt", "r") as file:
+        for line_number, line in enumerate(file, start=1):
+            print(f"Line {line_number}: {line.strip()}")
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
+
---
+++ Task_8.py
@@ -0,0 +1,16 @@
+text = input("Enter text to write to the file: ")
+with open('output.txt', 'w') as file:
+    file.write(text + " \n")
+
+print("Data successfully written to output.txt")
+
+additional_text = input("Enter additional text to append: ")
+with open('output.txt', 'a') as file:
+    file.write(additional_text  +  " \n")
+
+print("Data successfully appended to output.txt")
+
+print("Final content of output.txt")
+with open('output.txt', 'r') as file:
+    content = file.read()
+    print(content)

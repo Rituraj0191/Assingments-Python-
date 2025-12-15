3139aecc8a616b98267f7e815553e860bada7a08
Rituraj0191 <146822205+Rituraj0191@users.noreply.github.com>
2025-12-15 23:06:47 +0530

    Add files via upload

---
+++ task_10.py
@@ -0,0 +1,9 @@
+numbers = (1,2,3,4,5,6,7,8,9,10)
+
+extracted_elements = numbers[:5]
+
+reversed_elements = extracted_elements[::-1]
+
+print("Original list:", numbers)
+print("Extracted first five elements:", extracted_elements)
+print("Reversed extracted elements:", reversed_elements)
